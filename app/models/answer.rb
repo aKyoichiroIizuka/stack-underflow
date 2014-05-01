7e4685ecc5b170f046bae1b52fe269f81f6e3605
Ryo Kajiwara <ryo.kajiwara@access-company.com>
2014-05-01 10:23:43 +0900

    associated question and answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   has_many :answer_votes
   has_many :answer_comments
+  belongs_to :question
   
   def total_points
     ans=self.answer_votes
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :question_votes
   has_many :question_comments
+
+  has_many :answers
 end
