ce3299d9347e8e60062f8106e22f0d009cbebd56
Ryo Kajiwara <ryo.kajiwara@access-company.com>
2014-05-02 14:35:50 +0900

    abbreviation helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
   def br(s)
-    s && h(s).gsub(/(\r\n?)|(\n)/, '<br />').html_safe
+	  s && h(s).gsub(/(\r\n?)|(\n)/, '<br />').html_safe
   end
   
   def rbr(s)
-    s && s.gsub(/(\r\n?)|(\n)/, '<br />').html_safe
+	  s && s.gsub(/(\r\n?)|(\n)/, '<br />').html_safe
+  end
+
+  def abbr(s, len = 30)
+	  if s.length > len
+	  	s[0..len - 1] + "..."
+	  else
+      s
+	  end
   end
 end
