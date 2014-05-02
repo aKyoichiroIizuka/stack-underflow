9e117dbad1577327ad8d21eb9646c7a1c9031a0b
aKyoichiro.Iizuka <Kyoichiro.Iizuka@access-company.com>
2014-05-02 14:49:59 +0900

    add answer vote test

--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -2,4 +2,8 @@ class AnswerVote < ActiveRecord::Base
   belongs_to :answer
   
   belongs_to :user
+  
+  validates :value, :numericality => {:only_integer => true, :greater_than => -2, :less_than => 2}
+  validates :user_id, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :answer_id, :numericality => {:only_integer => true, :greater_than => 0}
 end
