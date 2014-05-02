3f07ae16cb6ef209008a923c75167887b10b6534
aKyoichiro.Iizuka <Kyoichiro.Iizuka@access-company.com>
2014-05-02 15:03:50 +0900

    add answer comment test

--- app/models/answer_comment.rb
+++ app/models/answer_comment.rb
@@ -2,4 +2,8 @@ class AnswerComment < ActiveRecord::Base
   belongs_to :answer
   
   belongs_to :user
+  
+  validates :content, :presence => true, :length => {:minimum => 1}
+  validates :user_id, :numericality => {:only_integer => true, :greater_than => 0}
+  validates :answer_id, :numericality => {:only_integer => true, :greater_than => 0}
 end
--- test/models/answer_comment_test.rb
+++ test/models/answer_comment_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class AnswerCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "find" do
+		assert_equal 1, AnswerComment.count, "count"
+		
+		ac=AnswerComment.find(1)
+		assert_equal ac.id, 1, "find id"
+		assert_equal ac.content, "answercomment1", "find content"
+		assert_equal ac.user_id, 1, "find user_id"
+		assert_equal ac.answer_id, 1, "find answer_id"
+	end
+	
+	test "true save" do
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id => 1})		
+		assert ac.save, "save answercomment"
+
+#		ac = Answer.new({:content => 0, :user_id => 1, :answer_id => 1})		
+#		assert_equal ac.save, true, "content is 0"
+#		ac = Answer.new({:content => 0.0, :user_id => 1, :answer_id => 1})		
+#		assert_equal ac.save, true, "content is 0.0"
+	end
+
+	test "not equal type save" do
+		ac = AnswerComment.new({:content => nil, :user_id => 1, :answer_id => 1})
+		assert_equal ac.save, false, "content is nil"
+				
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => nil, :answer_id => 1})
+		assert_equal ac.save, false, "user_id is nil"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 0.0, :answer_id => 1})
+		assert_equal ac.save, false, "user_id is 0.0"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => "u", :answer_id => 1})
+		assert_equal ac.save, false, 'user_id is "u"'
+		
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id => nil})
+		assert_equal ac.save, false, "question_id is nil"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id => 0.0})
+		assert_equal ac.save, false, "question_id is 0.0"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id => "a"})
+		assert_equal ac.save, false, 'question_id is "a"'
+	end
+
+	test "false save" do
+		ac = AnswerComment.new({:content => "", :user_id => 1, :answer_id => 1})		
+		assert_equal ac.save, false, 'content is ""'
+		
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 0, :answer_id =>1})
+		assert_equal ac.save, false, "user_id is 0"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => -1, :answer_id =>1})
+		assert_equal ac.save, false, "user_id is -1"
+		
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id =>0})
+		assert_equal ac.save, false, "question_id is 0"
+		ac = AnswerComment.new({:content => "answercomment1", :user_id => 1, :answer_id =>-1})
+		assert_equal ac.save, false, "question_id is -1"
+	end
 end
